e52676510788232961c604f1d38f4e54081c407b
AleksandrBut <alex638000@gmail.com>
2019-12-10 19:19:00 +0200

    test LocalDateTime

---
+++ src/main/java/com/ai164/motionsensor/Main.java
@@ -0,0 +1,11 @@
+package com.ai164.motionsensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ src/main/java/com/ai164/motionsensor/controller/EventController.java
@@ -0,0 +1,19 @@
+package com.ai164.motionsensor.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class EventController {
+    @RequestMapping("/")
+    public void event() {
+        LocalDateTime time = LocalDateTime.now();
+        System.out.println("time.getYear() = " + time.getYear());
+        System.out.println("time.getMonth() = " + time.getMonthValue());
+        System.out.println("time.getDayOfMonth() = " + time.getDayOfMonth());
+        System.out.println("time.getHour() = " + time.getHour());
+        System.out.println("time.getMinute() = " + time.getMinute());
+    }
+}
