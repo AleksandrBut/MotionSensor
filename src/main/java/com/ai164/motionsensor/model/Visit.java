d5f34fa3b85cceb4547e7b041b9e86ba6c02a43e
AleksandrBut <alex638000@gmail.com>
2019-12-11 16:35:18 +0200

    add LocalDateTime field to Visit.java

--- src/main/java/com/ai164/motionsensor/model/Visit.java
+++ src/main/java/com/ai164/motionsensor/model/Visit.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
@@ -13,21 +14,10 @@ public class Visit {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    private int year;
-    private int month;
-    private int day;
-    private int hour;
+    private LocalDateTime time;
     private int visitCounter;
 
     public Visit() {
-    }
-
-    public Visit(int year, int month, int day, int hour, int visitCounter) {
-        this.year = year;
-        this.month = month;
-        this.day = day;
-        this.hour = hour;
-        this.visitCounter = visitCounter;
     }
 
     public Long getId() {
@@ -38,36 +28,12 @@ public class Visit {
         this.id = id;
     }
 
-    public int getYear() {
-        return year;
+    public LocalDateTime getTime() {
+        return time;
     }
 
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public int getMonth() {
-        return month;
-    }
-
-    public void setMonth(int month) {
-        this.month = month;
-    }
-
-    public int getDay() {
-        return day;
-    }
-
-    public void setDay(int day) {
-        this.day = day;
-    }
-
-    public int getHour() {
-        return hour;
-    }
-
-    public void setHour(int hour) {
-        this.hour = hour;
+    public void setTime(LocalDateTime time) {
+        this.time = time;
     }
 
     public int getVisitCounter() {
@@ -79,32 +45,26 @@ public class Visit {
     }
 
     @Override
+    public String toString() {
+        return "Visit{" +
+                "id=" + id +
+                ", time=" + time +
+                ", visitCounter=" + visitCounter +
+                '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Visit visit = (Visit) o;
-        return year == visit.year &&
-                month == visit.month &&
-                day == visit.day &&
-                hour == visit.hour &&
-                visitCounter == visit.visitCounter &&
-                Objects.equals(id, visit.id);
+        return visitCounter == visit.visitCounter &&
+                Objects.equals(id, visit.id) &&
+                Objects.equals(time, visit.time);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, year, month, day, hour, visitCounter);
-    }
-
-    @Override
-    public String toString() {
-        return "Visit{" +
-                "id=" + id +
-                ", year=" + year +
-                ", month=" + month +
-                ", day=" + day +
-                ", hour=" + hour +
-                ", visitCounter=" + visitCounter +
-                '}';
+        return Objects.hash(id, time, visitCounter);
     }
 }
