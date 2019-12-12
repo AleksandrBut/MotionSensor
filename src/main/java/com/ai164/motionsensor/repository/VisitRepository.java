9c60ba9094d6765f3f6563d688031d2a96d9e4d3
AleksandrBut <alex638000@gmail.com>
2019-12-12 22:17:27 +0200

    add usage of Transactional annotation

--- src/main/java/com/ai164/motionsensor/repository/VisitRepository.java
+++ src/main/java/com/ai164/motionsensor/repository/VisitRepository.java
@@ -2,9 +2,11 @@ package com.ai164.motionsensor.repository;
 
 import com.ai164.motionsensor.model.Visit;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 public interface VisitRepository extends CrudRepository<Visit, Long> {
     Visit findVisitByYearAndMonthAndDayAndHour(int year, int month, int day, int hour);
 
